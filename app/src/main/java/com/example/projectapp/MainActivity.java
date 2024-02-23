f584af53115e8d48c19c56434242a4c58144c4db
Dang Le <nguyenh1@ualberta.ca>
2024-02-22 17:48:36 -0700

    update

--- app/src/main/java/com/example/projectapp/MainActivity.java
+++ app/src/main/java/com/example/projectapp/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.projectapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button createEventButton = findViewById(R.id.createEventButton);
+        Button joinEventButton = findViewById(R.id.joinEventButton);
+
+        createEventButton.setOnClickListener(view -> {
+
+        });
+
+
     }
 }
