b2bcee1e1ff7ac38d6dd2292a42db2d1dd6d3e71
veerpm <vmehra@ualberta.ca>
2024-03-08 15:09:06 -0700

    small updates

--- app/src/test/java/com/example/projectapp/AttendeeTest.java
+++ app/src/test/java/com/example/projectapp/AttendeeTest.java
@@ -3,6 +3,8 @@ package com.example.projectapp;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
+import java.util.HashMap;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -52,12 +54,27 @@ class AttendeeTest {
     }
 
     @Test
-    void testEventsGetterAndSetter() {
+    void testSignedEventsGetterAndSetter() {
         ArrayList<String> newEvents = new ArrayList<>();
         newEvents.add("Event1");
         newEvents.add("Event2");
-        attendee.setEvents(newEvents);
-        assertEquals(newEvents, attendee.getEvents(), "Events lists do not match");
-        assertTrue(attendee.getEvents().contains("Event1") && attendee.getEvents().contains("Event2"), "Events list does not contain the correct events");
+        attendee.setSignedUpEvents(newEvents);
+        assertEquals(newEvents, attendee.getSignedUpEvents(), "Events lists do not match");
+        assertTrue(attendee.getSignedUpEvents().contains("Event1") && attendee.getSignedUpEvents().contains("Event2"), "Events list does not contain the correct events");
+    }
+
+    @Test
+    void testCheckedEventsGetterAndSetter() {
+        HashMap<String, Integer> newEvents = new HashMap<>();
+        newEvents.put("Event1", 2);
+        newEvents.put("Event2", 3);
+        attendee.setCheckedInEvents(newEvents);
+        assertEquals(newEvents, attendee.getCheckedInEvents(), "Events lists do not match");
+        assertTrue(attendee.getCheckedInEvents().containsKey("Event1") && attendee.getCheckedInEvents().containsKey("Event2"), "Events list does not contain the correct events");
+    }
+
+    @Test
+    void testAddCheckedEvent(){
+
     }
 }
