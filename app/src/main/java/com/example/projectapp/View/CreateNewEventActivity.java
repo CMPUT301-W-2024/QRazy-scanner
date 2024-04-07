aee949a7468d822638092f63746bc85d7a6d641d
Lameuta <giasonnguyen995@gmail.com>
2024-04-07 02:30:24 -0600

    Forget 1 thing

--- app/src/main/java/com/example/projectapp/View/CreateNewEventActivity.java
+++ app/src/main/java/com/example/projectapp/View/CreateNewEventActivity.java
@@ -245,7 +245,7 @@ public class CreateNewEventActivity extends AppCompatActivity implements AddEven
      *      Base64 encoded string of the bitmap.
      */
     public String bitmapToString(Bitmap bitmap) {
-        int maxSize = 1024; // Maximum dimension (width or height) for the resized bitmap
+        int maxSize = 3072; // Maximum dimension (width or height) for the resized bitmap
         int width = bitmap.getWidth();
         int height = bitmap.getHeight();
 
